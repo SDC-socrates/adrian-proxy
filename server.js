2ed29fda46c3d7bdaefe9b862ff65fa647040684
Adrian Khoo <adriankhoo.ca@gmail.com>
2019-01-24 21:23:28 -0800

    Working with turashc component

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
+require('newrelic');
 const express = require('express');
-const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3004;
+const port = process.env.PORT || 3005;
 
-app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(/\/\d+\//, express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
